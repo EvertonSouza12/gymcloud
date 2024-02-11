9b9897f41261ea535100ded7bafb87671c4025e7
Everton Souza <souza1287@live.com>
2024-02-10 23:11:33 -0300

    html templates

--- src/gymcloud/pages/urls.py
+++ src/gymcloud/pages/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='gymcloud-home'),
+    path('gymcloud/login', views.login, name='gymcloud-login'),
+    path('gymcloud/register', views.register, name='gymcloud-register'),
+    path('gymcloud/setings/acount', views.setings, name='gymcloud-setings'),
+    path('gymcloud/workouts/list', views.workouts, name='gymcloud-list-workouts'),
+    path('gymcloud/workouts/setings/manage/workouts', views.manage_workouts, name='gymcloud-manage-workouts'),
 ]
--- src/gymcloud/pages/views.py
+++ src/gymcloud/pages/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def setings(request):
+    return render(request, 'setings.html')
+
+def manage_workouts(request):
+    return render(request, 'manage_workouts.html')
+
+def workouts(request):
+    return render(request, 'workouts.html')
