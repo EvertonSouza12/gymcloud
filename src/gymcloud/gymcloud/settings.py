c962603e346067395dd1651193815e5e1da871b4
Everton Souza <souza1287@live.com>
2024-02-12 18:41:53 -0300

    fix sqlite

--- src/gymcloud/gymcloud/settings.py
+++ src/gymcloud/gymcloud/settings.py
@@ -88,15 +88,10 @@ WSGI_APPLICATION = 'gymcloud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
--- src/gymcloud/users/models.py
+++ src/gymcloud/users/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-
 class user(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20, null=True)
