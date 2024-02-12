9e8f5be3934c442204de2561781dd745542e05fe
Everton Souza <souza1287@live.com>
2024-02-12 17:46:03 -0300

    update user model

--- src/gymcloud/users/models.py
+++ src/gymcloud/users/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class user(models.Model):
     id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20, null=True)
     first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
-    age = models.IntegerField(max_length=100)
+    age = models.IntegerField()
     email = models.EmailField()
     password = models.CharField()
--- src/gymcloud/users/views.py
+++ src/gymcloud/users/views.py
@@ -2,13 +2,22 @@ from django import forms
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.forms import UserCreationForm
+from .models import user
 
-class register(forms.Form):
+class register(UserCreationForm):
+    username = forms.CharField(label='Your username', max_length=20)
     first_name = forms.CharField(label='Your name', max_length=100)
     second_name = forms.CharField(label='Your name', max_length=100)
-    age = forms.IntegerField(label='Your age', max_value=110)
+    age = forms.IntegerField(label='Your age')
     email = forms.EmailInput(label='Your e-mail')
     password = forms.CharField(label='Your password', max_length=32, widget=forms.PasswordInput)
+    
+    class RegisterForm(UserCreationForm):
+    
+        class Meta:
+            model = user
+            fields = ['first_name','second_name','age', 'email', 'password']
     
     
 def register_user(request):
@@ -16,7 +25,7 @@ def register_user(request):
         form = register(request.POST)
         if form.is_valid():
             register.password = make_password(register.password)
-            return HttpResponseRedirect('/thanks')
+            return HttpResponseRedirect('')
     else:
         form = register()
     return render (request, 'register.html')
