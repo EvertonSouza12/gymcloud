d616e1badf1bb0dbe9b467a393ce5d0276bc9569
Everton Souza <souza1287@live.com>
2024-02-13 19:05:08 -0300

    remove files

--- src/gymcloud/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('gymcloud/login', views.login, name='gymcloud-login'),
-    path('gymcloud/register', views.register, name='gymcloud-register'),
-]
